261f52188b26e6a8d7c1b4ad2df830435f80a508
Mofeid Moshreky <mof_ton@yahoo.com>
2023-03-06 14:04:41 -0500

    copmlete lap

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+   return a + b;
+} 
+function subtract(a,b) {
+   return a - b;
+}
+function multiply(a,b) {
+   return a*b;
+}
+function divide(a,b) {
+   return a / b;
+}
+ function increment(n){
+   return ++n;
+ }
+function preserveDecimal (str){
+   return parseFloat(str);
+}
+function decrement(num){
+   return num -1;
+}
+function subtract(num1, num2){
+   return num1 - num2;
+}
+function makeInt(str){
+   return parseInt(str,10);
+}
